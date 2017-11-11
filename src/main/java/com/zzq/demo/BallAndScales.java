7449b41bcc6a6450b5cb71f2b6caf7682110eb83
yhsyzzq <yhsyzzq1987@163.com>
2017-11-11 11:32:50 +0800

    修复bug

--- src/main/java/com/zzq/demo/BallAndScales.java
+++ src/main/java/com/zzq/demo/BallAndScales.java
@@ -1,5 +1,7 @@
 package com.zzq.demo;
 
+import sun.security.ssl.Debug;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -18,13 +20,69 @@ public class BallAndScales {
     //小球集合
     private static List<Ball> balls = new ArrayList<Ball>();
 
+    private static Ball actualSpecialBall = null;
+
     //天平对象
     private static Scales scales;
 
-    public static void init() {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10000; i++) {
+            System.out.println("=============================== 我是分割线 ===================================");
+            BallAndScales.init(i+1);
+            Ball specialBall = BallAndScales.searchSpecialBall();
+            System.out.println("第" + (i+1) + "次实验结果：质量不同的小球编号：" + specialBall.getCode() + "， 重量：" + specialBall.getWeight());
+            if(actualSpecialBall.getCode() != specialBall.getCode() || actualSpecialBall.getWeight() != specialBall.getWeight()){
+                System.out.println("实验结果异常，请检查程序！");
+            }
+            System.out.println("=============================== 我是分割线 ===================================\r\n");
+
+        }
+
+        //检查bug调试代码
+//        System.out.println("=============================== 我是分割线 ===================================");
+//        BallAndScales.init2();
+//        Ball specialBall = BallAndScales.searchSpecialBall();
+//        System.out.println("实验结果：质量不同的小球编号：" + specialBall.getCode() + "， 重量：" + specialBall.getWeight());
+//        if (actualSpecialBall.getCode() != specialBall.getCode() || actualSpecialBall.getWeight() != specialBall.getWeight()) {
+//            System.out.println("实验结果异常，请检查程序！");
+//        }
+//        System.out.println("=============================== 我是分割线 ===================================\r\n");
+    }
+
+    public static void init2() {
+        balls.clear();
+        System.out.println("实验产生的12个小球的基本情况");
+        for (int i = 1; i <= DEFAULT_INITIAL_CAPACITY; i++) {
+            Ball ball = new Ball();
+            ball.setCode(i);
+            if (i == 2) {
+                ball.setWeight(1.0);
+            } else {
+                ball.setWeight(DEAFAULT_INITIAL_WEIGHT);
+            }
+            balls.add(ball);
+            System.out.println("====小球编号：" + ball.getCode() + "小球重量：" + ball.getWeight());
+        }
+
+        for (Ball ball : balls) {
+            if (ball.getWeight() != DEAFAULT_INITIAL_WEIGHT) {
+                actualSpecialBall = ball;
+                System.out.println("实验预期结果：质量不同的小球编号：" + ball.getCode() + ", 小球重量：" + ball.getWeight());
+            }
+        }
+        //初始化天平
+        scales = Scales.newSingleInstance();
+    }
+
+    public static void init(int n) {
+        balls.clear();
         //初始化小球
-        int randomCode = new Random().nextInt(13);
+        int randomCode = new Random().nextInt(12) + 1; //生成1-12的随机数
         double specialWeight = BallAndScales.setSpecialWeight();
+        if (specialWeight == 10.0) {
+            System.out.println("断点===10");
+        }
+        System.out.println("第" + n + "次实验产生的12个小球的基本情况,randomCode="+randomCode+",specialWeight="+specialWeight);
         for (int i = 1; i <= DEFAULT_INITIAL_CAPACITY; i++) {
             double weight = randomCode == i ? specialWeight : DEAFAULT_INITIAL_WEIGHT;
 
@@ -32,7 +90,13 @@ public class BallAndScales {
             ball.setCode(i);
             ball.setWeight(weight);
             balls.add(ball);
-            System.out.println("====小球编号：" + ball.getCode() + ", 小球重量：" + ball.getWeight());
+            System.out.println("小球编号：" + ball.getCode() + ", 小球重量：" + ball.getWeight());
+        }
+        for (Ball ball : balls) {
+            if (ball.getWeight() != DEAFAULT_INITIAL_WEIGHT) {
+                actualSpecialBall = ball;
+                System.out.println("第" + n + "次实验预期结果：质量不同的小球编号：" + ball.getCode() + ", 小球重量：" + ball.getWeight());
+            }
         }
 
         //初始化天平
@@ -47,7 +111,7 @@ public class BallAndScales {
     private static double setSpecialWeight() {
         double weight = new Random().nextInt(SPECIAL_BALL_MAX_WEIGHT);
         if (weight == DEAFAULT_INITIAL_WEIGHT || weight == 0) {
-            setSpecialWeight();
+            weight = setSpecialWeight();
         }
         return weight;
     }
@@ -69,7 +133,7 @@ public class BallAndScales {
             //============继续测试================//
             //步骤1-1:1-3号球放入天平左侧，9-11号球放入天平右侧
             leftBalls = balls.subList(0, 3);
-            rightBalls = specialBalls.subList(8, 11);
+            rightBalls = balls.subList(8, 11);
             status = scales.testBalance(leftBalls, rightBalls);
             //===============重量相等，重量不一致的球在12号球=================
             if (status == ScalesStatus.balance) {
@@ -77,9 +141,9 @@ public class BallAndScales {
                 return specialBall;
             } else if (status == ScalesStatus.turnLeft) {
                 //=========9-11号球有一个轻了========
-                leftBalls.clear();
+                leftBalls = new ArrayList<Ball>();
                 leftBalls.add(balls.get(8));
-                rightBalls.clear();
+                rightBalls = new ArrayList<Ball>();
                 rightBalls.add(balls.get(9));
                 status = scales.testBalance(leftBalls, rightBalls);
                 //步骤1-1-1:9号球放入天平左侧，10号球放入天平右侧
@@ -98,9 +162,9 @@ public class BallAndScales {
                 }
             } else {
                 //=========9-11号球有一个重了========
-                leftBalls.clear();
+                leftBalls = new ArrayList<Ball>();
                 leftBalls.add(balls.get(8));
-                rightBalls.clear();
+                rightBalls = new ArrayList<Ball>();
                 rightBalls.add(balls.get(9));
                 status = scales.testBalance(leftBalls, rightBalls);
                 //步骤1-1-1:9号球放入天平左侧，10号球放入天平右侧
@@ -109,12 +173,12 @@ public class BallAndScales {
                     specialBall = balls.get(10);
                     return specialBall;
                 } else if (status == ScalesStatus.turnLeft) {
-                    //====== 10号球较重 =====
-                    specialBall = balls.get(9);
-                    return specialBall;
-                } else {
                     //====== 9号球较重 =====
                     specialBall = balls.get(8);
+                    return specialBall;
+                } else {
+                    //====== 10号球较重 =====
+                    specialBall = balls.get(9);
                     return specialBall;
                 }
             }
@@ -177,7 +241,7 @@ public class BallAndScales {
                     //此种情况不可能发生，因为1号球较重
                 }
             } else {
-                //根据假设法推断 5,6,7号球有一个重了
+                //根据假设法推断 5,6,7号球有一个轻了
                 leftBalls = new ArrayList<Ball>();
                 leftBalls.add(balls.get(4));
                 rightBalls = new ArrayList<Ball>();
@@ -185,16 +249,16 @@ public class BallAndScales {
                 status = scales.testBalance(leftBalls, rightBalls);
 
                 if (status == ScalesStatus.balance) {
-                    //====== 7号球较重 =====
+                    //====== 7号球较轻 =====
                     specialBall = balls.get(6);
                     return specialBall;
                 } else if (status == ScalesStatus.turnLeft) {
-                    //====== 5号球较重 =====
-                    specialBall = balls.get(4);
+                    //====== 6号球较轻 =====
+                    specialBall = balls.get(5);
                     return specialBall;
                 } else {
-                    //====== 6号球较重 =====
-                    specialBall = balls.get(5);
+                    //====== 5号球较轻 =====
+                    specialBall = balls.get(4);
                     return specialBall;
                 }
             }
@@ -247,7 +311,7 @@ public class BallAndScales {
                     return specialBall;
                 } else if (status == ScalesStatus.turnRight) {
                     //====== 1号球较轻 =====
-                    specialBall = balls.get(5);
+                    specialBall = balls.get(0);
                     return specialBall;
                 } else {
                     //====== 此种情况不可能发生 =====
@@ -271,18 +335,12 @@ public class BallAndScales {
                     return specialBall;
                 } else {
                     //====== 2号球较轻 =====
-                    specialBall = balls.get(0);
+                    specialBall = balls.get(1);
                     return specialBall;
                 }
             }
         }
         return null;
-    }
-
-    public static void main(String[] args) {
-        BallAndScales.init();
-        Ball specialBall = BallAndScales.searchSpecialBall();
-        System.out.println("推断的质量不同的小球编号：" + specialBall.getCode() + "， 重量：" + specialBall.getWeight());
     }
 
 }
